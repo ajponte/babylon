fbeb5accf718db6991588a267de400af7e60e0f2
Alan Ponte <alanjponte@gmail.com>
2025-08-30 21:16:44 -0700

    add session creation to db driver

--- server/db.py
+++ server/db.py
@@ -1,9 +1,10 @@
 """Database driver."""
-
+import logging
 from typing import Any
-from flask import Flask
+from flask import Flask, g
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import Session
 
 DATABASE_EXTENSION_KEY = "db"
 SESSION_APP_CTX_KEY = "_session"
@@ -42,3 +43,18 @@ class Database:
             self._engine.dispose()
 
         flask_app.extensions[DATABASE_EXTENSION_KEY] = self
+
+    def get_session(self) -> Session:
+        """
+        Fetch an existing session, which can only be tied to a variable
+        in the Flask App's global namespace. If no such session variable
+        exists, create a new session, and cache the binding.
+
+        :return: An existing SQLAlchemy session, or a newly created one.
+        """
+        session = g.get(SESSION_APP_CTX_KEY, None)
+        if not session:
+            logging.info('No session object cached. Creating a new one')
+            session = sessionmaker(bind=self._engine)
+
+        return session
