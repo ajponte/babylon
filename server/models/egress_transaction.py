29572e56ac8778bb00e78c303af59748d0ed664b
Alan Ponte <alanjponte@gmail.com>
2025-08-30 18:35:41 -0700

    add initial classes

---
+++ server/models/egress_transaction.py
@@ -0,0 +1,25 @@
+# pylint: disable=too-few-public-methods
+"""Represents egress, (money out) from a specific account."""
+from typing import Optional
+from datetime import datetime, date
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.types import Enum, Text, String, Integer
+from server.models import BASE, create_random_uuid_hex, EgressTransactionSource
+
+
+class EgressTransaction(BASE):
+    """EgressTransaction ORM mapper."""
+
+    __tablename__ = "egress_transaction"
+    id: Mapped[str] = mapped_column(primary_key=True, default=create_random_uuid_hex)
+    date_created: Mapped[datetime] = mapped_column(default=datetime.now())
+    date_updated: Mapped[datetime] = mapped_column(default=datetime.now())
+    date_posted: Mapped[date] = mapped_column(nullable=False)
+
+    source = mapped_column(Enum(EgressTransactionSource), nullable=False)
+
+    description: Mapped[str] = mapped_column(String(30), nullable=False)
+
+    slip_number: Optional[Mapped[str]] = mapped_column(Text)
+
+    amount: Mapped[int] = mapped_column(Integer, nullable=False)
---
+++ server/models/ingress_transaction.py
@@ -0,0 +1,25 @@
+# pylint: disable=too-few-public-methods
+"""Represents egress, (money in) from a specific account."""
+from typing import Optional
+from datetime import datetime, date
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.types import Enum, Text, String, Integer
+from server.models import BASE, create_random_uuid_hex, IngressTransactionSource
+
+
+class EgressTransaction(BASE):
+    """IngressTransaction ORM mapper."""
+
+    __tablename__ = "egress_transaction"
+    id: Mapped[str] = mapped_column(primary_key=True, default=create_random_uuid_hex)
+    date_created: Mapped[datetime] = mapped_column(default=datetime.now())
+    date_updated: Mapped[datetime] = mapped_column(default=datetime.now())
+    date_posted: Mapped[date] = mapped_column(nullable=False)
+
+    source = mapped_column(Enum(IngressTransactionSource), nullable=False)
+
+    description: Mapped[str] = mapped_column(String(30), nullable=False)
+
+    slip_number: Optional[Mapped[str]] = mapped_column(Text)
+
+    amount: Mapped[int] = mapped_column(Integer, nullable=False)
