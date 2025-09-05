06bf99473a04ede7d63a200d74ec15abae22cb69
Alan Ponte <alanjponte@gmail.com>
2025-09-05 15:55:45 -0700

    update tx api

--- server/controllers/transaction.py
+++ server/controllers/transaction.py
@@ -5,19 +5,31 @@ from typing import Any
 from server.services.transaction_history import TransactionHistoryHandler
 
 
-async def transaction_history(start: int, end: int) -> tuple[dict[str, Any], int]:
+async def transaction_history(
+    transaction_type,
+    start: int,
+    end: int
+) -> tuple[dict[str, Any], int]:
     if not (start < end):
         message = f'{start} >= {end}'
         return {'message': message}, HTTPStatus.BAD_REQUEST
     try:
         logging.debug(f'Fetching transaction between {start} and {end}')
-        resp = _transaction_search(start, end)
+        resp = _transaction_search(
+            start,
+            end
+        )
         return {'transactions': resp}, HTTPStatus.OK
     except Exception as e:
         message = f'Unknown exception while fetching transaction history between {start}, {end}.'
         logging.info(message, exc_info=e)
         return {'message': message}, HTTPStatus.INTERNAL_SERVER_ERROR
 
-def _transaction_search(start: int, end: int):
-    handler = TransactionHistoryHandler(start=start, end=end)
+def _transaction_search(
+    transaction_type: str,
+    start: int,
+    end: int
+):
+    handler = TransactionHistoryHandler(transaction_type=transaction_type,
+                                        start=start, end=end)
     return handler.fetch_transaction_history()
--- server/services/transaction_history.py
+++ server/services/transaction_history.py
@@ -1,8 +1,15 @@
 from server.db import get_session
 
 class TransactionHistoryHandler:
-    __slots__ = "_start", "_end", "_history"
-    def __init__(self, *, start: int, end: int):
+    __slots__ = "_transaction_type", "_start", "_end", "_history"
+    def __init__(
+        self,
+        *,
+        transaction_type: str,
+        start: int,
+        end: int
+    ):
+        self._transaction_type = transaction_type
         self._start: int = start
         self._end: int = end
         self._history = []
